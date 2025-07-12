241ccff092e091581e26a80d89b5c4784123dc3b
neo-zip <neo@neotap.net>
2025-07-11 20:49:25 -0400

    Quick start

--- install.sh
+++ install.sh
@@ -1,13 +1,8 @@
-# cpx
-
-
-## Get started
-
-```bash
 wget -O - https://apt.llvm.org/llvm.sh | sudo bash -s -- 19 all
 sudo apt install clang-19
 
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt update
 sudo apt install -y g++-13 libstdc++6
-```
+
+./cpx build clean

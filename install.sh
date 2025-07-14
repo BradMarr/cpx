bafd5ea524d2ee217182fad0a3b653799786e3e7
Bradley Marranca <bradmarranca@gmail.com>
2025-07-13 21:08:32 -0400

    install script

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
-wget -O - https://apt.llvm.org/llvm.sh | sudo bash -s -- 19 all
-sudo apt install clang-19
+#!/bin/bash
+sudo apt install -y clang-19
 
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt update
 sudo apt install -y g++-13 libstdc++6
 
-./cpx build clean
+sudo curl -L $(curl -s https://api.github.com/repos/BradMarr/cpx/releases/latest \
+    | grep "browser_download_url" \
+    | grep -E '/cpx"' \
+    | cut -d '"' -f 4) -o /usr/local/bin/cpx
+
+sudo chmod +x /usr/local/bin/cpx

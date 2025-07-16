d501afcdae10a959858e017e2cdd9a19ea2ed4a0
Bradley Marranca <bradmarranca@gmail.com>
2025-07-16 19:50:35 -0400

    simplify curl command for install script

--- install.sh
+++ install.sh
@@ -5,9 +5,6 @@ sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt update
 sudo apt install -y g++-13 libstdc++6
 
-sudo curl -L $(curl -s https://api.github.com/repos/BradMarr/cpx/releases/latest \
-    | grep "browser_download_url" \
-    | grep -E '/cpx"' \
-    | cut -d '"' -f 4) -o /usr/local/bin/cpx
+sudo curl -L https://github.com/BradMarr/cpx/releases/latest/download/cpx -o /usr/local/bin/cpx
 
 sudo chmod +x /usr/local/bin/cpx

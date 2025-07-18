fc7ccda735e8955f0d6a185b580e56c4c108a2ec
Bradley Marranca <bradmarranca@gmail.com>
2025-07-18 19:34:17 -0400

    minor error safety changes to install script

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+set -e
+
+sudo apt update
 sudo apt install -y clang-19
 
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt update
 sudo apt install -y g++-13 libstdc++6
 
-sudo curl -L https://github.com/BradMarr/cpx/releases/latest/download/cpx -o /usr/local/bin/cpx
+sudo curl --fail -L https://github.com/BradMarr/cpx/releases/latest/download/cpx -o /usr/local/bin/cpx
 
 sudo chmod +x /usr/local/bin/cpx
